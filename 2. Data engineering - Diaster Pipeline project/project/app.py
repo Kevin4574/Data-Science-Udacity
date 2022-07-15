92becbbc3c803168c932b63344443fa7ed51faa3
kevin <yezehua1234@gmail.com>
2022-07-15 14:26:02 +0800

    new

---
+++ 2. Data engineering - Diaster Pipeline project/project/app.py
@@ -0,0 +1,135 @@
+import json
+import plotly
+import pandas as pd
+import re
+
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+from flask import Flask
+from flask import render_template, request, jsonify
+from plotly.graph_objs import Bar
+import joblib
+from sqlalchemy import create_engine
+
+from sklearn.base import BaseEstimator, TransformerMixin
+
+path = r'D:\金融股票\git\Data-Science-Udacity\2. Data engineering - Diaster Pipeline project\project\data'
+
+app = Flask(__name__)
+
+# define a function to tokenize and clean the feature
+def tokenize(text):
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+# Build a custom transformer which will extract the starting verb of a sentence
+class toarray(BaseEstimator, TransformerMixin):
+
+    # Given it is a tranformer we can return the self
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return pd.DataFrame(X.toarray())
+
+class length(BaseEstimator, TransformerMixin):
+
+    # Given it is a tranformer we can return the self
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+
+        return pd.DataFrame([len(txt) for txt in X])
+# load data
+engine = create_engine('sqlite:///' + path + '\ETL_Cleaned.db')
+df = pd.read_sql_table('message', engine)
+
+# load model
+model = joblib.load(path + '\Best_Model')
+
+# index webpage displays cool visuals and receives user input text for model
+@app.route('/')
+@app.route('/index')
+def index():
+
+    # extract data needed for visuals
+    # TODO: Below is an example - modify to extract data for your own visuals
+    genre_counts = df.groupby('genre').count()['message']
+    genre_names = list(genre_counts.index)
+
+    # create visuals
+    # TODO: Below is an example - modify to create your own visuals
+    graphs = [
+        {
+            'data': [
+                Bar(
+                    x=genre_names,
+                    y=genre_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Genres',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Genre"
+                }
+            }
+        }
+    ]
+
+    # encode plotly graphs in JSON
+    ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+
+    # render web page with plotly graphs
+    return render_template('master.html', ids=ids, graphJSON=graphJSON)
+
+
+# web page that handles user query and displays model results
+@app.route('/go')
+def go():
+    # save user input in query
+    query = request.args.get('query', '')
+
+    # use model to predict classification for query
+    classification_labels = model.predict([query])[0]
+    classification_results = dict(zip(df.columns[4:], classification_labels))
+
+    # This will render the go.html Please see that file.
+    return render_template(
+        'go.html',
+        query=query,
+        classification_result=classification_results
+    )
+
+
+def main():
+    app.run(host='0.0.0.0', port=3000, debug=True)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
